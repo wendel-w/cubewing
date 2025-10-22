f0019b8be8a659189ab1d946f92fdc9cf097abeb
vendel <vendel.tout@yandex.com>
2025-10-22 20:45:16 +0300

    auto commit

--- components/AlgGrid/AlgGrid.js
+++ components/AlgGrid/AlgGrid.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import styles from "./AlgGrid.module.css";
 import ColorSchemeSelector from "../ColorSchemeSelector/ColorSchemeSelector";
+import Cube from "cubejs/lib/cube";
 export default function AlgGrid({algSource, SvgComponent}) {
     const [algset, setAlgset] = useState({});
     const [colorScheme, setColorScheme] = useState({});
@@ -10,6 +11,9 @@ export default function AlgGrid({algSource, SvgComponent}) {
         .then(res => res.json())
         .then(data => setAlgset(data));
     }, []);
+    function createLinkAlg(alg) {
+        return alg.split("'").join("-").split(" ").join("_");
+    }
     return (
         <>
             <ColorSchemeSelector colorScheme={colorScheme} setColorScheme={setColorScheme} />
@@ -19,15 +23,25 @@ export default function AlgGrid({algSource, SvgComponent}) {
                         <div className={styles.categoryContainer} key={category}>
                             <h2 className={styles.categoryTitle}>{category}</h2>
                             <ul className={styles.caseContainer}>
-                            {algs.map(({ name, alg }) => (
+                            {algs.map(({ name, alg }) =>{ 
+                                const cleanAlg = alg.split("’").join("'").split("(").join("").split(")").join("").split("2'").join("2");
+                                const inverseCleanAlg = Cube.inverse(cleanAlg);
+                                const linkAlg = createLinkAlg(cleanAlg);
+                                const linkInverseAlg = createLinkAlg(inverseCleanAlg);
+
+                                return(
                                 <li key={name} className={styles.case}>
-                                    <SvgComponent className={styles.svg} algorithm={alg.split("’").join("'").split("(").join("").split(")").join("").split("2'").join("2")} colorScheme={colorScheme} />
+                                    <a className={styles.a} target="_blank" rel="noopener noreferrer" href={`https://alg.cubing.net/?setup=${linkInverseAlg}&alg=${linkAlg}&stage=L6E`}>
+                                        <SvgComponent className={styles.svg} algorithm={cleanAlg} colorScheme={colorScheme} />
+                                    </a>
                                     <div className={styles.caseInfo}>
                                         <p className={styles.caseTitle}>{name}:</p>
                                         <p className={styles.caseAlg}>{alg}</p>
                                     </div>
                                 </li>
-                            ))}
+                                );
+                            }
+                            )}
                             </ul>
                         </div>
                     ))
